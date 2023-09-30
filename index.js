044eecff2c89dc13ad66b11ab46210efde12290b
Glen <garpirungu@gmail.com>
2023-09-30 16:28:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
-}
+    // Get a reference to the input element by its ID
+    const input = document.getElementById('input');
+  
+    // Define the event listener function
+    function handleClick() {
+      alert('Input was clicked!');
+    }
+  
+    // Add a click event listener to the input element
+    input.addEventListener('click', handleClick);
+  }
+  
+  // Call the addingEventListener function to set up the event listener
+  addingEventListener();
+  
